510d37ded279fbe82c2e0b164277e6901695084a
Meriç Akgül <39882155+mericakgul@users.noreply.github.com>
2022-04-04 22:52:35 +0200

    Initial commit, home page added

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+const Home = () => {
+    return (
+        <div>
+            <h2>Upcoming Events</h2>
+            <dl>
+                <dt>August 4</dt>
+                <dd>NYSL Fundraiser</dd>
+                <dt>August 16</dt>
+                <dd>Season Kick-off: Meet the Teams</dd>
+                <dt>September 1</dt>
+                <dd>First Game of the Season (Check Game Schedule for details)</dd>
+            </dl>
+        </div>
+    );
+}
+
+export default Home;

0109d3fab9455a0446e41028240099b0f93927db
Meriç Akgül <39882155+mericakgul@users.noreply.github.com>
2022-05-07 02:04:33 +0200

    google map has been added to the game details page

--- src/components/Games.js
+++ src/components/Games.js
@@ -7,31 +7,56 @@ const locations = details['locations'];
 
 export const Games = () => {
     const navigate = useNavigate();
-    let id = useParams().id;
+    const id = useParams().id;
+    const locationName = locations[games[id]['location']]['name'];
+    const locationAddress = locations[games[id]['location']]['address'];
+    const locationUrl = locations[games[id]['location']]['url'];
     return (
         <div>
             <h1>Details For The Game - {id}</h1>
             <table className="table-striped table-hover table-bordered table">
                 <tbody>
+                <tr>
+                    <th scope="row">Date</th>
+                    <td className="date" align="center">{games[id]['date']}</td>
+                </tr>
+                <tr>
+                    <th scope="row">Teams</th>
+                    <td align="center">{games[id]['teams']}</td>
+                </tr>
+                <tr>
+                    <th scope="row">Location</th>
+                    <td align="center">{locationName}</td>
+                </tr>
+                <tr>
+                    <th scope="row">Time</th>
+                    <td align="center">{games[id]['time']}</td>
+                </tr>
+                </tbody>
+            </table>
+            <h2>Game Location:</h2>
+            <table className="table">
+                <thead>
                     <tr>
-                        <th scope="row">Date</th>
-                        <td className="date" align="center">{games[id]['date']}</td>
+                        <th scope="col">Location Name</th>
+                        <th scope="col">Address</th>
+                        <th scope="col">Map</th>
                     </tr>
+                </thead>
+                <tbody>
                     <tr>
-                        <th scope="row">Teams</th>
-                        <td align="center">{games[id]['teams']}</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Location</th>
-                        <td align="center">{locations[games[id]['location']]['name']}</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Time</th>
-                        <td align="center">{games[id]['time']}</td>
+                        <td>{locationName}</td>
+                        <td>{locationAddress}</td>
+                        <td>
+                            <div>
+                                <iframe src={locationUrl} title= "Location map" width="400" height="300" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"/>
+                            </div>
+                        </td>
                     </tr>
                 </tbody>
             </table>
-            <button onClick={() => navigate('/schedule')}>Back to the Schedule Page</button>
+
+            <button type="button" className="btn btn-secondary" onClick={() => navigate('/schedule')}>Back to the Schedule Page</button>
         </div>
     );
 }

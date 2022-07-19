8db84ea948b3d3a238e13a2408ba5697beb03aec
Meriç Akgül <39882155+mericakgul@users.noreply.github.com>
2022-07-20 00:18:57 +0200

    Message page layout has been added

--- src/components/MessagePage.js
+++ src/components/MessagePage.js
@@ -2,9 +2,41 @@ import React from 'react';
 
 const MessagePage = ({gameId}) => {
     return (
-        <p>
-            This is the message page for the game {gameId}
-        </p>
+        <>
+            <h2>Messages of game {gameId}</h2>
+            <div className="list-group">
+                <div className="list-group-item list-group-item-action" aria-current="true">
+                    <div className="d-flex w-100 justify-content-between">
+                        <div className="d-flex">
+                            <h5 className="mb-1">Author Name</h5>
+                            <small className="text-muted">(email)</small>
+                        </div>
+                        <small>Message sent time</small>
+                    </div>
+                    <p className="mb-1">Message content</p>
+                </div>
+                <div className="list-group-item list-group-item-action" aria-current="true">
+                    <div className="d-flex w-100 justify-content-between">
+                        <div className="d-flex">
+                            <h5 className="mb-1">Author Name</h5>
+                            <small className="text-muted">(email)</small>
+                        </div>
+                        <small>Message sent time</small>
+                    </div>
+                    <p className="mb-1">Message content</p>
+                </div>
+                <div className="list-group-item list-group-item-action" aria-current="true">
+                    <div className="d-flex w-100 justify-content-between">
+                        <div className="d-flex">
+                            <h5 className="mb-1">Author Name</h5>
+                            <small className="text-muted">(email)</small>
+                        </div>
+                        <small>Message sent time</small>
+                    </div>
+                    <p className="mb-1">Message content</p>
+                </div>
+            </div>
+        </>
     );
 };
 

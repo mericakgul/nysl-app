7bdea56bda1d02d02e735fde50ab536869e042ec
Meriç Akgül <39882155+mericakgul@users.noreply.github.com>
2022-07-31 17:23:16 +0200

    moving common used function to helpers.js file

--- src/utilities/helpers.js
+++ src/utilities/helpers.js
@@ -12,3 +12,7 @@ const adjustTimeSyntax = (time) => {
 export const sortMessagesFromOldToNew = (messages) => {
         return messages.sort((first, second) => Object.values(first)[0]["timestamp"] - Object.values(second)[0]["timestamp"]);
 }
+
+export const onSubmitHandler = (event) => {
+    event.preventDefault();
+}
